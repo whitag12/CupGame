c4d4fde66c154be5914b2fbec27c43e1ffb760ee
nzayatz14 <nzayatz14@hotmail.com>
2014-03-20 16:34:10 -0400

    Initial Commits CupGame

---
+++ src/pkg3710/project2/Project2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg3710.project2;
+
+/**
+ *
+ * @author nzayatz14
+ */
+public class Project2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new NewJFrame().setVisible(true);
+    }
+}
